d5d8c879d85c78922ce71cccc87b5637209e9458
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-03-17 11:26:07 +0100

    add: layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-function App() {
+export default function App() {
   return (
     <>
       <h1>Setup Web App React</h1>
     </>
   )
-}
-
-export default App
+};
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>Setup Web App React</h1>
+        </header>
+    )
+};
